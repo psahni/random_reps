630c5c1bcaa84188c32bb1b1790c0c5fe1d11941
Prashant Sahni <prashant.sahni5@gmail.com>
2014-06-01 20:10:11 +0530

    planbit app

---
+++ PlanBit/test/controllers/asset_managers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AssetManagersControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ PlanBit/test/helpers/asset_managers_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class AssetManagersHelperTest < ActionView::TestCase
+end
